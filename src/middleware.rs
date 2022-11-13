f3bb94deefe7db355d95205709e5661a017f8f6b
elsayedz <elsayed.z@aucegypt.edu>
2022-11-13 02:18:10 +0200

    Election algorithm working

--- src/middleware.rs
+++ src/middleware.rs
@@ -2,8 +2,13 @@ use elections::election_client::ElectionClient;
 use elections::InitElectionRequest;
 use elections::Empty;
 
-use std::time::Duration;
+use rand::Rng;
+use rand::SeedableRng;
+use rand::rngs::OsRng;
+
+use std::env;
 use std::thread::sleep;
+use std::time::Duration;
 
 pub mod elections {
     tonic::include_proto!("elections");
@@ -11,48 +16,76 @@ pub mod elections {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let args: Vec<String> = env::args().collect();
     
-    let mut client = ElectionClient::connect("http://192.168.1.12:50051").await?;
-    let mut fallenServer = ElectionClient::connect("http://192.168.1.12:50053").await?;
+    let ip1 = format!("{}{}","http://" ,&args[1]);
+    let ip2 = format!("{}{}","http://" ,&args[2]);
+    let ip3 = format!("{}{}","http://" ,&args[3]);
     
     
-    let request = tonic::Request::new(
-        Empty{
-           message: String::from("Test"),   //to_owned() --> take a copy from the data
+    
+    
+    let client_1 = ElectionClient::connect(ip1).await?;
+    let client_2 = ElectionClient::connect(ip2).await?;
+    let client_3 = ElectionClient::connect(ip3).await?;
+    // let mut fallenServer = ElectionClient::connect("http://10.40.32.92:50054").await?;
+    
+    let mut servers_connections = vec![client_1.clone(),client_2.clone(),client_3.clone()];
+    let mut servers_connections_2 = vec![client_1.clone(),client_2.clone(),client_3.clone()];
+    
+
+
+    // let mut request = tonic::Request::new(
+    //     InitElectionRequest{
+    //         id: "2".to_owned(),   //to_owned() --> take a copy from the data
+    //     }
+    // );
+    // let response = client.init_election(request).await;
+    // println!("Init Election={:?}", response);
+
+
+    tokio::task::spawn(async move {
+        let mut interval = tokio::time::interval(std::time::Duration::from_secs(6));
+        loop {
+            interval.tick().await;
+            let mut rng = ::rand::rngs::StdRng::from_seed(OsRng.gen());
+            let random_index = rng.gen_range(0..=servers_connections.len());
+            println!("Thread spawned");
+            let mut request = tonic::Request::new(
+                InitElectionRequest{
+                    id: "".to_owned(),  
+                }
+            );
+            request.set_timeout(Duration::from_secs(1));
+            let response = servers_connections[random_index].init_election(request).await;
+            println!("Sent request to server {}", random_index);
+            println!("Init election response={:?}", response);
         }
-    );
-    let response = client.ping(request).await;
-    println!("Ping Response={:?}", response);
-
-
-    let mut request = tonic::Request::new(
-        InitElectionRequest{
-            id: "2".to_owned(),   //to_owned() --> take a copy from the data
-        }
-    );
-    let response = client.init_election(request).await;
-    println!("RESPONSE={:?}", response);
-
+    });
+    let mut i =0;
    loop {
-        println!("Loop started");
-        // let ten_sec = time::Duration::from_millis(2000);
-        sleep(Duration::from_millis(10000));
-        println!("I'm alive");
-        let request = tonic::Request::new(
-            InitElectionRequest{
-                id: "2".to_owned(),   //to_owned() --> take a copy from the data
-            }
-        );
-        let response = client.init_election(request).await;
-        println!("RESPONSE={:?}", response);
-
-        let request = tonic::Request::new(
+        let mut request = tonic::Request::new(
             Empty{
-               message: String::from("Test"),   //to_owned() --> take a copy from the data
+               message: String::from("Test"),
             }
         );
-        let response = fallenServer.ping(request).await;
-        println!("RESPONSE From Fallen server={:?}", response);
+        request.set_timeout(Duration::from_millis(1000));
+        let res = servers_connections_2[i].ping(request).await
+        .unwrap_or(tonic::Response::new(Empty{message: String::from("Server is down")}));
+        
+        println!("Ping Response from server {} ={:?}", i ,res);
+        i = (i+1)%3;
+        sleep(Duration::from_millis(2000));
+        
+        
+        // let response_1 = client_1.ping(request).await;
+        // println!("Ping Response={:?}", response_1);
+        
+        // let response_2 = client_2.ping(request).await;
+        // println!("Ping Response={:?}", response_1);
+        
+        // let response_2 = client_3.ping(request).await;
+        // println!("Ping Response={:?}", response_1);
     }
     
 }
--- src/server.rs
+++ src/server.rs
@@ -4,8 +4,12 @@ use elections::election_server::{Election, ElectionServer};
 use elections::election_client::ElectionClient;
 use elections::{Empty, InitElectionRequest, BroadcastId};
 
+use std::time::Duration;
+use std::thread::sleep;
+
 use std::collections::HashMap;
 use std::env;
+use std::net::{SocketAddr};
 use std::sync::{Arc, Mutex};
 use http::Uri;
 
@@ -20,7 +24,7 @@ pub struct MyElection {
     // pub my_server: MyServer
     // value : Arc<Mutex<bool>>,
     pub id_to_ip : HashMap<String, String>,
-    pub isWorking : Arc<Mutex<bool>>
+    pub is_working : Arc<Mutex<bool>>
 }
 
 #[derive(Debug, Default)]
@@ -30,11 +34,8 @@ pub struct MyServer {
 
 #[tonic::async_trait]
 impl Election for MyElection {
-
-    async fn init_election(
-        &self,
-        request: Request<InitElectionRequest>,
-    ) -> Result<Response<Empty>, Status> {
+    
+    async fn init_election(&self, request: Request<InitElectionRequest>) -> Result<Response<Empty>, Status> {
         let id = request.into_inner().id;
         println!("Got a init_election request: {:?}", &id);
 
@@ -42,16 +43,24 @@ impl Election for MyElection {
             message: format!("ACK!").into(),
         };
 
-        let mut client = ElectionClient::connect(self.id_to_ip.get(&id).unwrap().parse::<Uri>().unwrap()).await.unwrap();
-        let request = tonic::Request::new(
+        let mut min_id = std::i32::MAX;
+        for (key, _value) in &self.id_to_ip {
+            if key.parse::<i32>().unwrap() < min_id {
+                min_id = key.parse::<i32>().unwrap();
+            }
+        }
+        println!("Min id: {:?}", min_id);
+        let mut client = ElectionClient::connect(self.id_to_ip.get(&min_id.to_string()).unwrap().parse::<Uri>().unwrap()).await.unwrap();
+        let mut request = tonic::Request::new(
             Empty {
                 message: format!("Time to go down").into(),
             }
         );
-    
+        
+        request.set_timeout(Duration::from_millis(1000));
         let response = client.force_failure(request).await?;
     
-        println!("RESPONSE={:?}", response);
+        println!("Force Failure Response ={:?}", response.get_ref().message);
 
         Ok(Response::new(reply))
     }
@@ -70,11 +79,15 @@ impl Election for MyElection {
         Ok(Response::new(reply))
     }
 
-    async fn force_failure(&self, request: Request<Empty>,) -> Result<Response<Empty>, Status> {
-        println!("Got force_failure request: {:?} \n This Server will GO DOWN!!", request);
+    async fn force_failure(&self, request: Request<Empty>) -> Result<Response<Empty>, Status> {
+        println!("Got force_failure request from: {:?} \n This Server will GO DOWN!!", request.remote_addr());
 
-        let mut mutValue = self.isWorking.lock().unwrap();
-        *mutValue = false;
+        sleep(Duration::from_millis(30000));
+        println!("Server is UP again");
+
+        let mut mut_value = self.is_working.lock().unwrap();
+        *mut_value = false;
+        
         let reply = elections::Empty {
             message: format!("").into(),
         };
@@ -86,16 +99,22 @@ impl Election for MyElection {
         &self,
         request: Request<Empty>,
     ) -> Result<Response<Empty>, Status> {
-        let value = self.isWorking.lock().unwrap();
-        if *value != false {
-            println!("Got ping request: {:?}", request);
+        // let value = self.is_working.lock().unwrap();
+        // if *value != false {
+        //     println!("Got ping request from: {:?}\n Message:{:?}", request.remote_addr() ,request.get_ref().message);
+        //     let reply = elections::Empty {
+        //         message: format!("").into(),
+        //     };
+        //     Ok(Response::new(reply))
+        // } else {
+        //     println!("Got ping request while server is down,");
+        //     Err(Status::unavailable("Server is down"))
+        // }
+        println!("Got ping request from: {:?}\n Message:{:?}", request.remote_addr() ,request.get_ref().message);
             let reply = elections::Empty {
                 message: format!("").into(),
             };
             Ok(Response::new(reply))
-        } else {
-            Err(Status::unavailable("Server is down"))
-        }
     }
 }    
 
@@ -106,14 +125,14 @@ impl MyElection {
         // vec.push(_ip2);
         // vec.push(_ip3);
 
-        let mut ipID: HashMap<String, String> = HashMap::new();
-        ipID.insert("1".to_string(), _ip1);
-        ipID.insert("2".to_string(), _ip2);
-        ipID.insert("3".to_string(), _ip3);
+        let mut ip_id: HashMap<String, String> = HashMap::new();
+        ip_id.insert("1".to_string(), _ip1);
+        ip_id.insert("2".to_string(), _ip2);
+        ip_id.insert("3".to_string(), _ip3);
 
         Self {
-                id_to_ip: ipID,
-                isWorking: Arc::new(Mutex::new(true))
+                id_to_ip: ip_id,
+                is_working: Arc::new(Mutex::new(true))
         }
     }
 }
@@ -125,18 +144,29 @@ impl MyElection {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = env::args().collect();
-    let ip = &args[1];
+    let ip_server1 = &args[1];
+    let ip_server2 = &args[2];
+    let ip_server3 = &args[3];
+    
+    let main_server_index = &args[4].parse::<usize>().unwrap();
+    
+    let servers = vec![ip_server1, ip_server2, ip_server3];
 
-    let addr = ip.parse()?;
+    let server_addr:SocketAddr = servers[*main_server_index].parse().unwrap();
+    let ip1 = format!("{}{}","http://" ,server_addr);
+    let ip2 = format!("{}{}","http://" ,ip_server2);
+    let ip3 = format!("{}{}","http://" ,ip_server3);
+    println!("Main server listening on: {}", servers[*main_server_index]);
+    // println!("Server2 listening on {}", ip2);
+    // println!("Server3 listening on {}", ip3);
 
     // let mut myServer = MyElection::new(ip.to_string(), String::from("http://10.40.32.92:50053"),
     // String::from("http://10.40.32.92:50054"));
 
-    let mut election_service = MyElection::new(ip.to_string(), String::from("http://192.168.1.12:50053"),
-    String::from("http://192.168.1.12:50054"));
+    let election_service = MyElection::new(ip1, ip2, ip3);
     Server::builder()
         .add_service(ElectionServer::new(election_service))
-        .serve(addr).await?;
+        .serve(server_addr).await?;
 
     Ok(())
 }
