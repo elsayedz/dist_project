62cd1a62356eab5038823e0cb7fa5231b1cff127
elsayedz <elsayed.z@aucegypt.edu>
2022-11-14 22:36:30 +0200

    Added broadcast_id functionality

--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,18 @@
+use tonic::transport::Channel;
 use tonic::{transport::Server, Request, Response, Status};
 
 use elections::election_server::{Election, ElectionServer};
 use elections::election_client::ElectionClient;
 use elections::{Empty, InitElectionRequest, BroadcastId};
 
+use futures::lock::Mutex;
 use std::time::Duration;
 use std::thread::sleep;
 
 use std::collections::HashMap;
 use std::env;
 use std::net::{SocketAddr};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
 use http::Uri;
 
 
@@ -19,18 +21,18 @@ pub mod elections {
 }
 
 
-#[derive(Debug, Default)]
+
+#[derive(Debug, Default, Clone)]
 pub struct MyElection {
     // pub my_server: MyServer
     // value : Arc<Mutex<bool>>,
-    pub id_to_ip : HashMap<String, String>,
-    pub is_working : Arc<Mutex<bool>>
+    // pub id_to_ip : HashMap<String, String>,
+    pub id_to_ip : Arc<Mutex<HashMap<String, String>>>,
+    pub is_working : Arc<Mutex<bool>>,
+    pub my_id : Arc<Mutex<String>>,
+    pub connections : Arc<Mutex<Vec<ElectionClient<Channel>>>>
 }
 
-#[derive(Debug, Default)]
-pub struct MyServer {
-    
-}
 
 #[tonic::async_trait]
 impl Election for MyElection {
@@ -44,38 +46,58 @@ impl Election for MyElection {
         };
 
         let mut min_id = std::i32::MAX;
-        for (key, _value) in &self.id_to_ip {
-            if key.parse::<i32>().unwrap() < min_id {
+        let mut map = self.id_to_ip.lock().await;
+
+        for (key, _value) in &*map {
+            if key.parse::<i32>().unwrap() > min_id {
                 min_id = key.parse::<i32>().unwrap();
             }
         }
         println!("Min id: {:?}", min_id);
-        let mut client = ElectionClient::connect(self.id_to_ip.get(&min_id.to_string()).unwrap().parse::<Uri>().unwrap()).await.unwrap();
-        let mut request = tonic::Request::new(
-            Empty {
-                message: format!("Time to go down").into(),
+        match map.get(&min_id.to_string()){
+            Some(ip) => {
+                println!("Min id: {:?}", min_id);
+                println!("Min id ip: {:?}", ip);
+                // let mut client = ElectionClient::connect(ip.parse::<Uri>().unwrap()).await;
+                let mut client = ElectionClient::connect(ip.parse::<Uri>().unwrap()).await.unwrap();
+                let mut request = tonic::Request::new(
+                    Empty {
+                        message: format!("Time to go down").into(),
+                    }
+                );
+                
+                request.set_timeout(Duration::from_millis(1000));
+                let response = client.force_failure(request).await?;
+                println!("Sent request to server {}", min_id);
+                println!("Broadcast id response={:?}", response);
+            },
+            None => {
+                println!("Couldn't find min id in Map");
             }
-        );
+        }
+        // let mut client = ElectionClient::connect(map.get(&min_id.to_string()).unwrap().parse::<Uri>().unwrap()).await.unwrap();
         
-        request.set_timeout(Duration::from_millis(1000));
-        let response = client.force_failure(request).await?;
     
-        println!("Force Failure Response ={:?}", response.get_ref().message);
+        // println!("Force Failure Response ={:?}", response.get_ref().message);
 
         Ok(Response::new(reply))
     }
 
     async fn broadcast_id(&self,request: Request<BroadcastId>,) -> Result<Response<Empty>, Status> {
-        println!("Got broadcast_id (new_id) request: {:?}", request);
+        println!("-----Got a broadcast_id (new_id) request------\n");
+        println!("Old id: {:?}", request.get_ref().old_id);
+        println!("New id: {:?}", request.get_ref().new_id);
+        println!("New ip: {:?}", request.get_ref().new_ip);
+
+        let mut ip_map = self.id_to_ip.lock().await;
+        ip_map.remove(&request.get_ref().old_id);
+        ip_map.insert(request.get_ref().new_id.clone(), request.get_ref().new_ip.clone());
+
 
         let reply = elections::Empty {
-            message: format!("").into(),
+            message: format!("Updated IP map Successfully").into(),
         };
-
-        let ip = request.remote_addr().unwrap().ip().to_string();
-        println!("IP: {}", ip);
-        // ip_to_id[ip] = request.request.id;
-
+        
         Ok(Response::new(reply))
     }
 
@@ -85,8 +107,40 @@ impl Election for MyElection {
         sleep(Duration::from_millis(30000));
         println!("Server is UP again");
 
-        let mut mut_value = self.is_working.lock().unwrap();
-        *mut_value = false;
+        let mut ip_map = self.id_to_ip.lock().await;
+        let mut my_id = self.my_id.lock().await;
+        
+        let my_ip = ip_map.get(&*my_id).unwrap().clone();   // My cuurent ip
+        let max_id = ip_map.keys().max().unwrap().clone();      // Max id in the network
+        ip_map.remove(&*my_id);                 // Remove my id from the map
+        
+        let id_as_int = max_id.parse::<i32>().unwrap();        // Convert max id to int
+        let new_id = id_as_int + 1;     // Increment max id by 1
+        println!("Removed myself from the map");
+
+
+        ip_map.insert(new_id.to_string(), my_ip.clone());
+        
+        let mut connection_vec = self.connections.lock().await;
+        
+        for i in 0..connection_vec.len() {
+            let mut request = tonic::Request::new(
+                BroadcastId {
+                    old_id: my_id.to_owned().clone(),
+                    new_id: new_id.to_string().clone(),
+                    new_ip: my_ip.clone(),
+                }
+            );
+            request.set_timeout(Duration::from_millis(1000));
+            let response = connection_vec[i].broadcast_id(request).await;
+            println!("Sent request to server {}", new_id);
+            println!("Broadcast id response={:?}", response);
+        }
+        
+        let mut update_id = self.my_id.lock().await;
+        *update_id = new_id.to_string();
+        // let mut mut_value = self.is_working.lock().unwrap();
+        // *mut_value = false;
         
         let reply = elections::Empty {
             message: format!("").into(),
@@ -114,25 +168,39 @@ impl Election for MyElection {
             let reply = elections::Empty {
                 message: format!("").into(),
             };
-            Ok(Response::new(reply))
+        Ok(Response::new(reply))
     }
 }    
 
 impl MyElection {
-    pub fn new(_ip1:String, _ip2:String, _ip3:String) -> Self {
+    pub async fn new(_ip1:String, _ip2:String, _ip3:String, _my_id:String) -> Self {
         // let mut vec = Vec::new();
         // vec.push(_ip1);
         // vec.push(_ip2);
         // vec.push(_ip3);
 
         let mut ip_id: HashMap<String, String> = HashMap::new();
-        ip_id.insert("1".to_string(), _ip1);
-        ip_id.insert("2".to_string(), _ip2);
-        ip_id.insert("3".to_string(), _ip3);
+        ip_id.insert("0".to_string(), _ip1.clone());
+        ip_id.insert("1".to_string(), _ip2.clone());
+        ip_id.insert("2".to_string(), _ip3.clone());
 
+        let mut servers_connections: Vec<ElectionClient<Channel>> = Vec::new();
+        println!("*********************");
+        for (key, value) in &ip_id {
+            if key != &_my_id {
+               servers_connections.push(ElectionClient::connect(value.parse::<Uri>().unwrap()).await.unwrap()); 
+               println!("Server id: {} --> IP {}", key, value);
+            }else{
+                println!("Main Server id: {} --> IP {}", key, value);
+            }
+        }
+        println!("*********************");
+        
         Self {
-                id_to_ip: ip_id,
-                is_working: Arc::new(Mutex::new(true))
+                id_to_ip: Arc::new(Mutex::new(ip_id)),
+                is_working: Arc::new(Mutex::new(true)),
+                my_id: Arc::new(Mutex::new(_my_id)),
+                connections: Arc::new(Mutex::new(servers_connections)),
         }
     }
 }
@@ -163,7 +231,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let mut myServer = MyElection::new(ip.to_string(), String::from("http://10.40.32.92:50053"),
     // String::from("http://10.40.32.92:50054"));
 
-    let election_service = MyElection::new(ip1, ip2, ip3);
+    let election_service = MyElection::new(ip1, ip2, ip3, main_server_index.to_string()).await;
     Server::builder()
         .add_service(ElectionServer::new(election_service))
         .serve(server_addr).await?;
